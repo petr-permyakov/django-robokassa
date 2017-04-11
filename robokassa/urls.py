d019b1fd6bcc4bc43bb2881bde764cbf16275145
Pavel Serov <kryptex@list.ru>
2017-04-11 17:41:08 +0300

    remove depr patterns()

--- robokassa/urls.py
+++ robokassa/urls.py
@@ -1,12 +1,15 @@
 # coding: utf-8
-try:
-    from django.conf.urls.defaults import patterns, url
-except ImportError:
-    from django.conf.urls import patterns, url
 
-urlpatterns = patterns(
-    'robokassa.views',
-    url(r'^result/$', 'receive_result', name='robokassa_result'),
-    url(r'^success/$', 'success', name='robokassa_success'),
-    url(r'^fail/$', 'fail', name='robokassa_fail'),
-)
+try:
+    from django.conf.urls.defaults import url
+except ImportError:
+    from django.conf.urls import url
+
+from . import views as v
+
+
+urlpatterns = [
+    url(r'^result/$', v.receive_result, name='robokassa_result'),
+    url(r'^success/$', v.success, name='robokassa_success'),
+    url(r'^fail/$', v.fail, name='robokassa_fail'),
+]
